c3ff83e59237bc9c1ec50b0d96c01ba8335f6f5d
Beppu Kaoru <beppu@f.ait.kyushu-u.ac.jp>
2013-07-12 22:05:22 +0900

    Add comment

--- vm/localization.c
+++ vm/localization.c
@@ -2,6 +2,14 @@
 #include "Common.h"
 #include "localization.h"
 
+/**********************************************************************************************	*/
+//	name:			localization
+//	Description:	自己位置推定関数
+//	Details:		left_moter:左モータのエンコーダ値 / right_moter:右モータのエンコーダ値
+//	Attention:		この関数を使用し始めるする前にinit_localization()を呼び出すこと
+//	Auther:			Kaoru　Beppu
+//	update:			2013.07.01
+/**********************************************************************************************	*/
 extern void localization(F32 left_moter, F32 right_moter) {
 	F32 phi_R;
 	F32 phi_L;
@@ -19,11 +27,10 @@ extern void localization(F32 left_moter, F32 right_moter) {
 	delta_L_L = 2 * PI * WHEEL_R * phi_L / 360;
 	delta_L = (delta_L_R + delta_L_L) / 2;
 	delta_theta = (delta_L_R - delta_L_L) / (2 * WHEEL_D);
-	//delta_theta = (delta_L_R) / (2 * WHEEL_D);
-	rho = delta_L / delta_theta;
+	rho = delta_L / delta_theta;							// delta_thetaの値が小さいとオーバーフローする
 	//rho = (phi_R + phi_L) * WHEEL_D / (phi_R - phi_L);
 
-	if(delta_theta < 0.0001) {
+	if(delta_theta < 0.0001) {								// オーバーフローを防ぐためdelta_thetaが小さい時は近似式
 		localization_x = localization_x + delta_L * cos(localization_theta + delta_theta);
 		localization_y = localization_y + delta_L * sin(localization_theta + delta_theta);
 		//line = 0;
@@ -34,7 +41,9 @@ extern void localization(F32 left_moter, F32 right_moter) {
 		localization_y = localization_y + delta_L * sin(localization_theta + delta_theta / 2);
 		//line = 1;
 	}
+
 	localization_theta = localization_theta + delta_theta;
+
 	/*
 	display_clear(1);
 	display_goto_xy(0, 0);
@@ -61,13 +70,40 @@ extern void localization(F32 left_moter, F32 right_moter) {
 	display_string("theta:");
 	display_int((int)delta_theta, 0);
 	*/
+
 	return;
 }
 
+/**********************************************************************************************	*/
+//	name:			init_localization
+//	Description:	自己位置推定の初期化関数
+//	Details:		自己位置推定を始める前に1度起動すること
+//	Auther:			Kaoru　Beppu
+//	update:			2013.07.11
+/**********************************************************************************************	*/
 extern void init_localization() {
 	pre_phi_R = 0.0;
 	pre_phi_L = 0.0;
 	localization_x = 0.0;
 	localization_y = 0.0;
 	localization_theta = 0.0;
+}
+
+/**********************************************************************************************	*/
+//	name:			init_localization
+//	Description:	自己位置推定の補正用関数
+//	Details:		x:補正したいx値 / y:補正したいy値 / mode:0ならばthetaも補正、それ以外ならthetaはそのまま
+//	Auther:			Kaoru　Beppu
+//	update:			2013.07.11
+/**********************************************************************************************	*/
+extern void correct_localization(F32 x, F32 y, F32 theta, F32 mode) {
+	if(mode == 0) {
+		localization_x = x;
+		localization_y = y;
+		localization_theta = theta;
+	}
+	else {
+		localization_x = x;
+		localization_y = y;
+	}
 }
--- vm/opos.c
+++ vm/opos.c
@@ -1,14 +1,15 @@
 #include "opos.h"
 #include <math.h>
 
-/******************************************************************************
-** 関数	|	unsigned long num2str(long num, char str[], unsigned long offset)
-** 概要	|	long型を文字列に変換する関数
-** 引数	|	num		:	変換する数字(型が違う場合long型に変換する)
-**		|	str		:	変換した文字列を格納する配列
-**		|	offset	:	文字列のオフセット(通常0)
-** 戻値	|	変換されて出来た文字列の文字数
-*******************************************************************************	*/
+/******************************************************************************	*/
+//	name:			opos
+//	Description:	目的値指定型推進機構
+//	Details:		x:移動したいx / y:移動したいy
+//					mode:0or1ならばマップ上の座標(x,y)へ移動、2or3ならばその場を原点とした(x,y)へ移動
+//	Attention:		modeの2or3は現状正しく動かないため修正の必要あり
+//	Auther:			Kaoru　Beppu & Yuya Miyamoto
+//	update:			2013.07.12
+/******************************************************************************	*/
 extern void opos(F32 x, F32 y, U16 mode) {
 	F32 target_theta;
 	F32 opos_theta;
@@ -22,28 +23,28 @@ extern void opos(F32 x, F32 y, U16 mode) {
 		x += localization_x;
 		y += localization_y;
 	}
-	target_theta = atan2(y-localization_y, x-localization_x);
+	target_theta = atan2(y-localization_y, x-localization_x);	// 目標地への角度を算出(atan2は-180<=θ<=180の値をとる)
 	if(mode == 2 || mode == 3) {
 		target_theta += localization_theta;
 	}
-	target_theta = target_theta * 180 / PI;
+	target_theta = target_theta * 180 / PI;						// rad -> deg
 
-	opos_theta = localization_theta;
-	opos_theta = opos_theta * 180 / PI;
-	opos_theta = (S32)opos_theta % 360;
+	opos_theta = localization_theta;							// 現在の角度を取得
+	opos_theta = opos_theta * 180 / PI;							// rad -> deg
+	opos_theta = (S32)opos_theta % 360;							// 360度を超えないようにする
 
-	if(opos_theta > 90 && opos_theta < 360 && target_theta < 0){
-		target_theta += 360;
+	if(opos_theta > 90 && opos_theta < 360 && target_theta < 0) {
+		target_theta += 360;									// target_thetaが180度以上の値をとるように補正
 	}
-	else if(opos_theta < -90 && opos_theta > -360 && target_theta > 0){
-		target_theta = 360 - target_theta;
+	else if(opos_theta < -90 && opos_theta > -360 && target_theta > 0) {
+		target_theta = 360 - target_theta;						// target_thetaが-180度以下の値をとるように補正
 	}
 
 	opos_dif = target_theta - opos_theta;
 	//opos_differential = opos_dif - opos_pre_dif;
 	//opos_integral += (opos_dif + opos_pre_dif) / 2.0 * 0.004;
 	//opos_turn = opos_P_gain * opos_dif + opos_I_gain * opos_integral + opos_D_gain * opos_differential;
-	opos_turn = opos_P_gain * opos_dif;
+	opos_turn = opos_P_gain * opos_dif;							// とりあえずP制御のみ
 
 	//opos_pre_dif = opos_dif;
 
@@ -69,6 +70,14 @@ extern void opos(F32 x, F32 y, U16 mode) {
 	return;
 }
 
+/******************************************************************************	*/
+//	name:			init_opos
+//	Description:	目的値指定型推進機構の初期化関数
+//	Details:		opos関数を使用し始める前に起動
+//	Attention:		今のところあまり意味なし(これから必要になるかも・・・)
+//	Auther:			Kaoru　Beppu
+//	update:			2013.07.12
+/******************************************************************************	*/
 void init_opos() {
 	opos_turn = 0.0;
 }
