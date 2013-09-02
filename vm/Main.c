9881c6e07346b054e4a01d2678a74715f5daca56
miyamoto yuya <temotemo1212@gmail.com>
2013-09-02 18:08:28 +0900

    haiiro kaizen4

--- vm/Main.c
+++ vm/Main.c
@@ -650,8 +650,6 @@ TASK(TaskSensor)
 */
 TASK(TaskActuator)
 {
-	float tail_forward = 0.0;
-	float tail_turn = 0.0;
 
 	/*--------------------------*/
 	/*	PWMの初期化				*/
@@ -732,31 +730,8 @@ TASK(TaskActuator)
 			&g_pwm_L,
 			&g_pwm_R
 		);*/
-		
-		tail_forward = (float)g_Actuator.forward * 0.625F;
-		tail_turn = (float)g_Actuator.turn * 0.275F;
-
-		g_pwm_L = (int)(tail_forward + tail_turn);
-		g_pwm_R = (int)(tail_forward - tail_turn);
-
-		if (g_pwm_L > 100) {
-			g_pwm_L = 100;
-		}
-		else if(g_pwm_L < -100) {
-			g_pwm_L = -100;
-		}
-
-		if(g_pwm_R > 100) {
-			g_pwm_R = 100;
-		}
-		else if(g_pwm_R < -100) {
-			g_pwm_R = -100;
-		}
-
-		/*
 		g_pwm_L = (g_Actuator.forward + g_Actuator.turn)/2 ;
 		g_pwm_R = (g_Actuator.forward - g_Actuator.turn)/2 ;
-		*/
 
 		/*if(abs(g_pwm_L) > 100)
 		{
